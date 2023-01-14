646ef73c81adf5e6f4c785fc46c96f75f9a6e5a0
Aleksandr Smirnov <grossmann.aleksandr@gmail.com>
2023-01-14 21:22:21 +0300

    fix

--- Qt/untitled/main.py
+++ Qt/untitled/main.py
@@ -1,12 +1,13 @@
+import os
 from matplotlib import pyplot as plt
 import numpy as np
 import sys
 
-price = []
-types = []
-
+price = [int(sys.argv[1])]
+types = [sys.argv[2]]
 
 fig = plt.figure(figsize =(10, 7))
 plt.pie(price, labels = types)
 
-plt.savefig("file.img")
+os.mkdir("img")
+plt.savefig("img/file.png") 
--- Qt/untitled/mainwindow.cpp
+++ Qt/untitled/mainwindow.cpp
@@ -7,22 +7,19 @@
 #include <cstdlib>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-    {
+    : QMainWindow(parent), 
+    ui(new Ui::MainWindow) {
     ui->setupUi(this);
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+MainWindow::~MainWindow() {
+delete ui;
 }
 
-void MainWindow::on_pushButton_clicked()
-{
-    std::filesystem::path path = "main.py";
-    QString str1 = ui -> lineEdit -> text();
-    QString str2 = ui -> lineEdit_2 -> text();
-    std::string command = "python " + path.u8string() + " " + str1.toStdString() + " " + str2.toStdString();
-    std::system(command.c_str());
+void MainWindow::on_pushButton_clicked() {
+std::filesystem::path path = "python/main.py";
+QString str1 = ui -> lineEdit_1 -> text();
+QString str2 = ui -> lineEdit_2 -> text();
+std::string command = "python " + path.u8string() + " " + str1.toStdString() + " " + str2.toStdString();
+std::system(command.c_str());
 }
